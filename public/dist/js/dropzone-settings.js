bd37f8cf3c76f3ff00f177b877f07523ce0128c0
Justin Oud <justin@vouchercompany.nl>
2015-04-23 10:26:58 +0200

    Added visual layer for images

--- public/dist/js/dropzone-settings.js
+++ public/dist/js/dropzone-settings.js
@@ -16,16 +16,26 @@ Dropzone.options.myDropzone = {
 //    previewTemplate: document.querySelector('#preview-template').innerHTML,
     paramName: "file", // The name that will be used to transfer the file
     dictDefaultMessage: '<div class="uploadbutton"><span class="glyphicon glyphicon-plus-sign"></span><p id="instructions">Drop your files to upload to ...</p></div>',
-    dictRemoveFile: 'Remove the file',
+    dictRemoveFile: '<span class="glyphicon glyphicon-remove"></span>',
+    thumbnailWidth: 100,
+    thumbnailHeight: 100,
     autoProcessQueue: false,
     addRemoveLinks: true,
     accept: function(file, done) {
-        if (file.name == "test.png") {
+        if (file.name == "..") {
             done("Naha, you don't.");
         }
         else {
             done();
+            if ($('.dz-preview').length) {
+                $('.dz-default').fadeIn();
+                $('.confirmation-box').fadeIn();
+            }
         }
 
     }
 };
+
+$(document).ready(function() {
+
+});
